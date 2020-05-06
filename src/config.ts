2bd94e0baa390225070ca1d41cf9432e5a882f61
Bryan Clark <clarkbw@github.com>
2020-05-06 10:37:08 -0700

    Drop unused variable

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,6 @@ import {join} from 'path';
 import * as core from '@actions/core';
 import {mkdirP} from '@actions/io';
 
-export const CONFIG_FILE = 'config.json';
-
 export async function config(): Promise<void> {
   // https://docs.docker.com/engine/reference/commandline/cli/#change-the-docker-directory
   const temp: string = process.env['RUNNER_TEMP'] || '';
