e885708aa0ecbf76ce9705f9ad3775fc26574ae0
Bryan Clark <clarkbw@github.com>
2020-07-08 15:14:46 -0700

    Use try / catch and throw for errors

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,26 @@
-import * as fs from 'fs';
 import {join} from 'path';
 
 import * as core from '@actions/core';
 import {mkdirP} from '@actions/io';
 
 export async function config(): Promise<void> {
-  // https://docs.docker.com/engine/reference/commandline/cli/#change-the-docker-directory
-  const temp: string = process.env['RUNNER_TEMP'] || '';
-  const dir: string = join(temp, `.docker-${Date.now()}`);
-
   try {
+    // https://docs.docker.com/engine/reference/commandline/cli/#change-the-docker-directory
+    const temp: string = process.env['RUNNER_TEMP'] || '';
+    const dir: string = join(temp, `.docker-${Date.now()}`);
+
     await mkdirP(dir);
 
     core.exportVariable('DOCKER_CONFIG', dir);
     console.log(`$DOCKER_CONFIG = ${dir}`);
-  } catch (e) {
-    console.error(`Error making directory ${dir}`, e);
-    return Promise.reject(e);
-  } finally {
+
     const experimental: string = core.getInput('experimental');
     if (experimental) {
       core.exportVariable('DOCKER_CLI_EXPERIMENTAL', 'enabled');
       console.log(`DOCKER_CLI_EXPERIMENTAL=enabled`);
     }
+  } catch (e) {
+    console.error(`Error setting DOCKER_CONFIG`, e);
+    throw e;
   }
 }
--- src/docker.ts
+++ src/docker.ts
@@ -1,17 +1,15 @@
-import * as core from '@actions/core';
+import {getInput, setSecret} from '@actions/core';
 import {exec} from '@actions/exec';
-import {join} from 'path';
-import {mkdirP} from '@actions/io';
 import {config} from './config';
 
 export async function docker(): Promise<number> {
-  const username: string = core.getInput('username', {required: true});
-  const password: string = core.getInput('password', {required: true});
-  const registry: string = core.getInput('registry');
-
-  core.setSecret(password); // should be a no-op but always do this to be safe
-
   try {
+    const username: string = getInput('username', {required: true});
+    const password: string = getInput('password', {required: true});
+    const registry: string = getInput('registry');
+
+    setSecret(password); // should be a no-op but always do this to be safe
+
     await config();
     // echo $TOKEN | docker login docker.pkg.github.com -u clarkbw --password-stdin
     try {
@@ -22,10 +20,12 @@ export async function docker(): Promise<number> {
       );
     } catch (e) {
       console.error(`Error logging into ${registry}`, e);
-      return Promise.reject(e);
+      throw e;
     }
+    return -100;
   } catch (e) {
     console.error(`Bailing on login attempt ${e}`);
-    return Promise.reject(e);
+    throw e;
   }
+  return -1000;
 }
