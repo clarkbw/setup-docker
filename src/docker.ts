01596ef5084c5b68979a5c6b467c3f0811a06b1f
Bryan Clark <clarkbw@github.com>
2020-08-20 14:51:27 -0700

    Add debugging

--- src/docker.ts
+++ src/docker.ts
@@ -1,4 +1,4 @@
-import {getInput, setSecret, addPath} from '@actions/core';
+import {getInput, setSecret, addPath, debug} from '@actions/core';
 import {exec} from '@actions/exec';
 import * as tc from '@actions/tool-cache';
 import {join} from 'path';
@@ -48,18 +48,25 @@ async function cli() {
   const RELEASE = 'stable'; // edge, nightly, test https://download.docker.com/mac/static/
   const DOCKER_URL = `https://download.docker.com/mac/static/${RELEASE}/${ARCHITECTURE}/docker-${DOCKER_VERSION}.tgz`;
   // https://github.com/actions/runner/blob/2709cbc0eae592e6004b016c52c04382f19c7683/src/Runner.Common/HostContext.cs#L243-L244
+  debug(`DOCKER_URL ${DOCKER_URL}`);
   const CACHE_DIR = process.env['RUNNER_TOOL_CACHE'] || '';
+  debug(`CACHE_DIR ${CACHE_DIR}`);
   const PATH = join(CACHE_DIR, 'docker', DOCKER_VERSION, ARCHITECTURE);
-
+  debug(`PATH ${PATH}`);
   const dockerPath = await tc.downloadTool(DOCKER_URL);
+  debug(`dockerpath ${dockerPath}`);
   const dockerExtractedFolder = await tc.extractTar(dockerPath, PATH);
+  debug(`dockerExtractedFolder ${dockerExtractedFolder}`);
   const cachedPath = await tc.cacheDir(
     dockerExtractedFolder,
     'docker',
     DOCKER_VERSION,
     ARCHITECTURE
   );
+  debug(`cachedPath ${cachedPath}`);
+  debug(`ENV ${process.env}`);
   addPath(cachedPath);
+  debug(`ENV ${process.env}`);
 }
 
 async function fetch(platform: string, architecture: string, version: string) {
